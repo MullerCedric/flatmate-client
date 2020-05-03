7eb0ed945f79540d22fa9dc98b96e739a8019fce
Cédric Müller <muller.cedric.96@gmail.com>
2020-05-03 20:40:23 +0200

    Update config to minimize compiled code

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,16 @@ module.exports = {
     // options...
     publicPath: process.env.NODE_ENV === 'production'
         ? '/flatmate-front/'
-        : '/'
+        : '/',
+    chainWebpack: config => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+                // modify the options...
+                options.compilerOptions = {whitespace: 'condense'};
+                return options;
+            });
+    },
 };
